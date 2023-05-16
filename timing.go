c86e782372cbffdc7d3b678d3fc988daa438699f
0xEclair <38656355+0xEclair@users.noreply.github.com>
2023-05-17 01:40:08 +0800

    fix: filename
    feat: Timing.Passed

--- timing.go
+++ timing.go
@@ -10,6 +10,13 @@ func (t *Timing) Start() {
 	t.Time = time.Now()
 }
 
+func (t *Timing) Passed() time.Duration {
+	dur := time.Since(t.Time)
+	t.Time = time.Now()
+
+	return dur
+}
+
 func (t *Timing) End() time.Duration {
 	return time.Since(t.Time)
 }
